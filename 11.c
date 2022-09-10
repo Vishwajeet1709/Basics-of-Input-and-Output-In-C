ed741d214a5b03836240c13fb7f21040afc52db2
Vishwajeet <67817041+Vishwajeet1709@users.noreply.github.com>
2022-09-10 13:40:47 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -4,7 +4,7 @@ int main()
     int time;
     printf("Enter time (HH:MM) : ");
     scanf("%d",&time);
-    printf("\n %d hour(s), and %d minut(s).",time/100,time%100);
+    printf("\n %d hour(s), and %d minut(s).\n\n",time/100,time%100);
     return 0;
 }
 
