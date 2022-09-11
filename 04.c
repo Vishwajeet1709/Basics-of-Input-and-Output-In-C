aa2f1f3256f14189316b7cdd6d3a233d67b08c8c
Vishwajeet <67817041+Vishwajeet1709@users.noreply.github.com>
2022-09-11 17:03:53 +0530

    Update 04.c

--- 04.c
+++ 04.c
@@ -1,13 +1,14 @@
+//Program to calculate area of circle 
 #include<stdio.h>
 int main()
 {
     float R,A;  //Here "A" stands for area, and "R" stands for radius
 
-    printf("Enter Radius to find Area : "); /*Asking for radius*/
+    printf("Enter Radius To Find Area Of Circle  : "); /*Asking for radius*/
     scanf("%f",&R);
 
     A=3.14*R*R;
 
-    printf("Area of circle is %f having the radius %f",A,R);
+    printf("Area of circle is %f having the radius %f.\n\n",A,R);
     return 0;
 }
